3226fcb2e3addf5ae199761fe977b88bab7ea370
willynasiff <willynasiff@gmail.com>
2018-12-03 15:20:26 -0300

    cambie cosas logicas

--- fuente/mostrarGastos.py
+++ fuente/mostrarGastos.py
@@ -24,7 +24,7 @@ def getGastosMensuales(mes):
   losGastos = []
 
   for row in c.execute(sentencia1):
-    identificador = str(row[0])
+    identificador = id(row[0])
     descripcion = str(row[1])
     importe = float(row[2])
     fecha = str(row[3])
@@ -35,28 +35,67 @@ def getGastosMensuales(mes):
         
 
 
-  sentencia2 = 'select sum(Importe) from Movimiento where fecha between "201{0}-{1}-01" and "201{0}-{1}-31" group by ID_Categoria'.format(ano, mes)
+  sentencia2 = 'select ID_Categoria, sum(Importe) from Movimiento where fecha between "201{0}-{1}-01" and "201{0}-{1}-31" group by ID_Categoria'.format(ano, mes)
 
 
   sumaGastos = []
+  row = None
+  sumador = 1
   for row in c.execute(sentencia2):
-    sumaGastosMios = float(row[0])
+    idCate = int(row[0])
+    sumaGastosMios = float(row[1])
 
-    sumaGastos.append(sumaGastosMios)
+    if sumador == idCate:
+      sumaGastos.append(sumaGastosMios)
+    else: 
+      restador = idCate - sumador 
+      for cada in range(restador):
+        sumaGastos.append(0)
+        sumador = sumador + 1 
+ 
+      sumaGastos.append(sumaGastosMios)
+
+    sumador = sumador + 1
+    
+
+
+    
+
+  if not row:
+    sumaGastos = [0, 0, 0, 0]
+  
+ 
+
+
+
+
 
    
-  sentencia3 = 'select sum(Importe) from Movimiento group by ID_Categoria'
+  sentencia3 = 'select ID_Categoria, sum(Importe) from Movimiento group by ID_Categoria'
 
   sumaGastosTotales = []
+  row = None
+  sumador = 1
   for row in c.execute(sentencia3):
-    sumaGastosMiosTotales = float(row[0])
+    idCate = int(row[0])
+    sumaGastosMiosTotales = float(row[1])
 
-    sumaGastosTotales.append(sumaGastosMiosTotales)
+    if sumador == idCate:
+      sumaGastosTotales.append(sumaGastosMiosTotales)
+    else: 
+      restador = idCate - sumador 
+      for cada in range(restador):
+        sumaGastosTotales.append(0)
+        sumador = sumador + 1 
+ 
+      sumaGastosTotales.append(sumaGastosMiosTotales)
 
+    sumador = sumador + 1
+   
+    
 
-
-
-
+  if not row:
+    sumaGastosTotales = [0, 0, 0, 0]
 
 
 
